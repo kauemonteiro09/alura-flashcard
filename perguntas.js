14f3cce8d634c1556932ff58a91317b01679d323
kauemonteiro09 <90470903+kauemonteiro09@users.noreply.github.com>
2024-10-04 23:11:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,46 @@
+criaCartao(
+  "História",
+  "Qual foi o primeiro homem a pisar na Lua?",
+  "Neil Armstrong."
+);
+criaCartao(
+  "Geografia",
+  "Qual é o maior país do mundo em extensão territorial?",
+  "Rússia."
+);
+criaCartao(
+  "Ciências",
+  "Qual é a principal função do sistema nervoso?",
+  "Controlar e coordenar todas as funções do corpo."
+);
+
+criaCartao(
+  "Arte",
+  "Qual é o movimento artístico caracterizado pela valorização da natureza e da emoção individual?",
+  "Romantismo."
+);
+criaCartao(
+  "Música",
+  'Qual instrumento musical é considerado o "rei dos instrumentos"?',
+  "Órgão."
+);
+criaCartao(
+  "Esporte",
+  "Qual país é considerado o berço do futebol?",
+  "Inglaterra."
+);
+criaCartao(
+  "Política",
+  "Qual é o principal órgão do poder executivo no Brasil?",
+  "Presidência da República."
+);
+criaCartao(
+  "Economia",
+  "O que é inflação?",
+  "Aumento generalizado e contínuo dos preços de bens e serviços em um determinado período."
+);
+criaCartao(
+  "Tecnologia",
+  "Qual foi a primeira rede social a ultrapassar 1 bilhão de usuários?",
+  "Facebook."
+);
